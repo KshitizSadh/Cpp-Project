9826d50876d176a71f2b96cf688bb3e3ffda5f39
KshitizSadh <142923024+KshitizSadh@users.noreply.github.com>
2024-04-14 14:31:08 +0530

    Update encryption.cpp

--- encryption.cpp
+++ encryption.cpp
@@ -1,13 +1,17 @@
-#include "encryption.h"
+#include  "encryption.h"
 #include <fstream>
 #include <cctype>
+#include <iostream>
 
 using namespace std;
 
-bool performMagic(string &content, bool encrypt) {
-    int shift = encrypt ? 4 : -4;
+bool performmagic(string& content, bool encrypt) {
+    int a;
+    int shift = encrypt ? a : -a;
+    cout<<"what is shift value";
+    cin >> a;
 
-    for (char &ch : content) {
+    for(char& ch: content) {
         if (isalpha(ch)) {
             char base = isupper(ch) ? 'A' : 'a';
             ch = static_cast<char>((ch - base + shift + 26) % 26 + base);
@@ -17,26 +21,27 @@ bool performMagic(string &content, bool encrypt) {
     return true;
 }
 
-bool encryptFile(const string &filename, bool encrypt) {
+bool encryptfile(const string& filename, bool encrypt) {
+    //open the file
     ifstream inFile(filename);
-    if (!inFile) {
+    if(inFile) {
         return false;
     }
-
+    // read the content of the file 
     string content((istreambuf_iterator<char>(inFile)), {});
     inFile.close();
 
-    if (performMagic(content, encrypt)) {
-        ofstream outFile(encrypt ? "Encrypted_" + filename : "Decrypted_" + filename);
-        if (!outFile) {
+    if(performmagic(content,encrypt)) {
+        //creatingg output file and writing the modufied content
+        ofstream outfile(encrypt ? "encrpyted_" + filename : "decrypted_" + filename);
+        if(outfile) {
             return false;
         }
 
-        outFile << content;
-        outFile.close();
+        outfile << content; 
 
-        return true;
+        outfile.close();
+
+        return true; 
     }
-
-    return false;
 }
